81f9ee4f55411f2deef1ce16ab2af641e43ada57
Syahraz Fiqar Alamsyah <syahrazfa@gmail.com>
2026-02-08 15:07:33 +0700

    Error Fix

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 
 app = Flask(__name__)
+
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
-@app.route("/webhook", methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
     signature = request.headers.get("X-Line-Signature")
     body = request.get_data(as_text=True)
@@ -20,12 +22,14 @@ def webhook():
 
     return "OK", 200
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="Bot is alive.")
     )
+
 
 if __name__ == "__main__":
     app.run(port=5000)
